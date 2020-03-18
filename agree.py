43951a5a1dbdb9c51dbe2d80121fee629e3143ae
thai-trung <thaitrung1983@gmail.com>
2020-03-18 13:54:20 +0700

    add agree.py source

---
+++ agree.py
@@ -0,0 +1,10 @@
+# Implement if, else in python
+
+answer = input("Do you agree?");
+
+if answer in ["y", "y"]:
+    print("Agreed!")
+elif answer in ["N", "n"]:
+    print("Not agreed!")
+else:
+    print("Not answer!")
